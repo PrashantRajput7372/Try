bd33d73ace58b833a6f1b1bc92b305b64278fa47
PrashantRajput7372 <prashantsingh113113@gmail.com>
2025-06-05 21:12:25 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function App() {
+  const path = window.location.pathname;
+
+  if (path === "/mypath") {
+    return <div>Opening App…</div>;
+  }
+
+  const handleOpen = () => {
+    window.location.href = "https://otp-frontend-seven.vercel.app/mypath";
+  };
+
+  return (
+    <div style={{ padding: "40px", textAlign: "center" }}>
+      <h1>Home Page</h1>
+      <button onClick={handleOpen}>Open App MahaDEV SAMBHU TRIPURARI</button>
+    </div>
+  );
+}
+
+export default App;
