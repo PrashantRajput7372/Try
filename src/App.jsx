8e902af023fe9baed94632baa6e1f23f2558bf4f
Prashant Singh <prashantsingh113113@gmail.com>
2025-06-09 14:52:27 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+
 import React from "react";
 
 export default function App() {
   const handleOpenApp = () => {
     window.location.href = "https://try-ecru-two.vercel.app/mypath"; // Update this with your domain
-     // window.location.href = "maadhaar://"; // Update this with your domain
+     
   };
-
+ const handleCustomSchema = () => {
+   
+     window.location.href = "maadhaar://"; // Update this with your domain
+  };
   return (
     <div style={{ padding: "40px", textAlign: "center" }}>
       <h1>Universal Link Test</h1>
@@ -21,6 +25,19 @@ export default function App() {
         }}
       >
         Open iOS App
+      </button>
+       <button
+        onClick={handleCustomSchema}
+        style={{
+          backgroundColor: "#007bff",
+          color: "white",
+          padding: "10px 20px",
+          fontSize: "18px",
+          borderRadius: "5px",
+          cursor: "pointer",
+        }}
+      >
+        custom schema button
       </button>
     </div>
   );
