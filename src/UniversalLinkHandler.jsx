47d8239764df8c269ee317e7145f79a6babe1569
Prashant Singh <prashantsingh113113@gmail.com>
2025-06-20 11:01:50 +0530

    Update UniversalLinkHandler.jsx

--- src/UniversalLinkHandler.jsx
+++ src/UniversalLinkHandler.jsx
@@ -7,8 +7,10 @@ const UniversalLinkHandler = () => {
   useEffect(() => {
     console.log("Universal Link Triggered:", window.location.href);
 
+    // Attempt to open the app by navigating to the universal link.
+    // The AASA file on your domain should enable iOS to associate the link with your app.
     const timer = setTimeout(() => {
-      console.log("Redirecting to App Store");
+      console.log("App not opened, redirecting to App Store");
       window.location.href = "https://apps.apple.com/app/id1435469474";
     }, 1500);
 
@@ -16,37 +18,72 @@ const UniversalLinkHandler = () => {
   }, []);
 
   return (
-
-  <div style={{ padding: 40, textAlign: "center" }}>
-    {/* Embedded spinner styles */}
-    <style>{`
-      .apple-spinner {
-        margin: 0 auto;
-        width: 40px;
-        height: 40px;
-        border-radius: 50%;
-        border: 4px solid #cfd0d1;
-        border-top-color: #000000;
-        animation: spin 0.8s linear infinite;
-      }
-
-      @keyframes spin {
-        to {
-          transform: rotate(360deg);
-        }
-      }
-    `}</style>
-
-    <div className="apple-spinner" />
-
-    <p style={{ marginTop: 20 }}>
-      Redirecting to mAadhaar App...
-      <br />
-      If redirection doesn't happen in 3 seconds,&nbsp;
-      <a href="https://apps.apple.com/app/id1435469474">click here</a>.
-    </p>
-  </div>
+    <div style={{ padding: 40, textAlign: "center" }}>
+      <h2>Redirecting to mAadhaar App...</h2>
+      <p>
+        If the redirection doesn’t happen automatically,{" "}
+        <a href="https://apps.apple.com/app/id1435469474">
+          click here to open App Store.
+        </a>
+      </p>
+    </div>
   );
 };
 
 export default UniversalLinkHandler;
+
+
+
+
+// import React, { useEffect } from "react";
+// import { useLocation } from "react-router-dom";
+
+// const UniversalLinkHandler = () => {
+//   const location = useLocation();
+
+//   useEffect(() => {
+//     console.log("Universal Link Triggered:", window.location.href);
+
+//     const timer = setTimeout(() => {
+//       console.log("Redirecting to App Store");
+//       window.location.href = "https://apps.apple.com/app/id1435469474";
+//     }, 1500);
+
+//     return () => clearTimeout(timer);
+//   }, []);
+
+//   return (
+
+//   <div style={{ padding: 40, textAlign: "center" }}>
+//     {/* Embedded spinner styles */}
+//     <style>{`
+//       .apple-spinner {
+//         margin: 0 auto;
+//         width: 40px;
+//         height: 40px;
+//         border-radius: 50%;
+//         border: 4px solid #cfd0d1;
+//         border-top-color: #000000;
+//         animation: spin 0.8s linear infinite;
+//       }
+
+//       @keyframes spin {
+//         to {
+//           transform: rotate(360deg);
+//         }
+//       }
+//     `}</style>
+
+//     <div className="apple-spinner" />
+
+//     <p style={{ marginTop: 20 }}>
+//       Redirecting to mAadhaar App...
+//       <br />
+//       If redirection doesn't happen in 3 seconds,&nbsp;
+//       <a href="https://apps.apple.com/app/id1435469474">click here</a>.
+//     </p>
+//   </div>
+//   );
+// };
+
+// export default UniversalLinkHandler;
