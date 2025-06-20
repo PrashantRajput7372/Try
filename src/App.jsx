09955fdc386e4d7685f057b1cf98b90bd32168b8
Prashant Singh <prashantsingh113113@gmail.com>
2025-06-20 11:02:48 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import MobilePrompt from "./MobilePrompt";
 import UniversalLinkHandler from "./UniversalLinkHandler";
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* This route will be targeted via the Universal Link */}
         <Route path="/extra-path-1/ulink" element={<UniversalLinkHandler />} />
+        
+        {/* Optional fallback route (or you can redirect all unknown paths to your universal link route) */}
         <Route
           path="*"
           element={
-            <>
-              <div style={{ padding: "40px", textAlign: "center" }}>
-                <h1>Home Page</h1>
-                <p>Current URL: {window.location.href}</p>
-              </div>
-              <MobilePrompt />
-            </>
+            <div style={{ padding: "40px", textAlign: "center" }}>
+              <h1>Universal Link Domain</h1>
+              <p>
+                This domain is solely used for hosting the apple-app-site-association file and handling deep link redirection.
+              </p>
+            </div>
           }
         />
       </Routes>
@@ -26,3 +27,34 @@ function App() {
 }
 
 export default App;
+
+
+
+// import React from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import MobilePrompt from "./MobilePrompt";
+// import UniversalLinkHandler from "./UniversalLinkHandler";
+
+// function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/extra-path-1/ulink" element={<UniversalLinkHandler />} />
+//         <Route
+//           path="*"
+//           element={
+//             <>
+//               <div style={{ padding: "40px", textAlign: "center" }}>
+//                 <h1>Home Page</h1>
+//                 <p>Current URL: {window.location.href}</p>
+//               </div>
+//               <MobilePrompt />
+//             </>
+//           }
+//         />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
